715e6bef07a60bb91a333974fce512d2df49ceba
Alejandro Cruz <hcruz0716@gmail.com>
2023-09-13 16:31:25 -0600

    refactorization

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,8 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <div class="mx-auto max-w-screen-xl px-4 py- sm:px-6 lg:px-8">
-      <nav class="flex justify-center gap-4 my-4 text-2xl underline">
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/orders">orders</RouterLink>
-        <RouterLink to="/providers">providers</RouterLink>
-      </nav>
-    </div>
-  </header>
-
   <RouterView />
 </template>
 
