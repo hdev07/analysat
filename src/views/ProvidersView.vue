a3c0722bd7022577ce1ed3b4146cb6678adae8ec
Alejandro Cruz <hcruz0716@gmail.com>
2023-09-13 17:36:27 -0600

    Add providers

--- src/views/ProvidersView.vue
+++ src/views/ProvidersView.vue
@@ -1,7 +1,27 @@
-<script setup></script>
-
 <template>
-  <div class="about">
-    <h2 class="text-4xl mt-3 text-center">Proveedores</h2>
+  <div>
+    <TableData
+      v-if="providers && providers.length > 0"
+      :title="title"
+      :headers="headers"
+      :data="providers"
+    />
+    <p v-else>Cargando datos o no hay datos disponibles.</p>
   </div>
 </template>
+
+<script setup>
+import TableData from '@/components/common/TableData.vue'
+import { useMyStore } from '@/store/myStore.js'
+import { onMounted } from 'vue'
+
+const title = 'Lista de Proveedores'
+const headers = ['Proveedor', 'Contacto', 'Teléfono', 'Email']
+
+const myStore = useMyStore()
+const providers = myStore.providers
+
+onMounted(async () => {
+  await myStore.loadProviders()
+})
+</script>
